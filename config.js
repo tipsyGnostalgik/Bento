de9aee5ca8f932ef01468658cc8d32274e19cd1b
Jadexie <150957752+tipsyGnostalgik@users.noreply.github.com>
2024-11-03 01:42:22 -0300

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ const CONFIG = {
 	twelveHourFormat: true,
 
 	// Greetings
-	greetingMorning: '☀️Good morning!',
-	greetingAfternoon: '🔥Good afternoon,',
-	greetingEvening: '✨Good evening,',
-	greetingNight: '💢Go to Sleep!',
+	greetingMorning: 'Good morning!',
+	greetingAfternoon: 'Good afternoon,',
+	greetingEvening: 'Good evening,',
+	greetingNight: 'Go to Sleep!',
 
 	// Layout
 	bentoLayout: 'lists', // 'bento', 'lists', 'buttons'
